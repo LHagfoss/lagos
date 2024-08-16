97193b6a71fc355b2e78f109ed94f06818d64e8a
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-08-16 16:07:04 +0200

    2nd

--- app/page.tsx
+++ app/page.tsx
@@ -29,19 +29,14 @@ export default function Home() {
                     <a href="#down"><Image src={down} alt="" className="w-[40px]"></Image></a>
                 </motion.div>
                 <motion.div initial={{ y: 50, scale: 0.7, opacity: 0 }} animate={{ y: 0, scale: 1, opacity: 1 }} transition={{ delay: 1.8, duration: 1, ease: easeInOut }} className="">
-                <div className="bg-[#34383c] hover:bg-[#4c5257] duration-300 mt-10 py-3 px-4 rounded-full cursor-pointer"><button type="button" className="text-[#e9ebec] bricolage-500 w-full h-full">Available for Work</button></div>
+                <div className="bg-[#34383c] hover:bg-[#4c5257] duration-300 mt-10 py-3 px-4 rounded-full cursor-pointer flex gap-2 items-center"><div className="w-[20px] h-[20px] bg-[#1c5e1d]"></div><div className=" absolute rounded-full w-[15px] h-[15px] bg-[#38c13b]"></div><button type="button" className="text-[#e9ebec] bricolage-500 w-full h-full">Available for Work</button></div>
                 </motion.div>
             </div>
-            <div id="down" className="w-screen h-screen"></div>
+            <div id="down" className="w-screen h-screen">
+
+            </div>
             <div className="w-screen h-screen flex flex-col justify-between">
-                <div className="w-full flex justify-between">
-                    <div className="w-[25px] h-[25px] bg-[#e9ebec] rounded-full"></div>
-                    <div className="w-[25px] h-[25px] bg-[#e9ebec] rounded-full"></div>
-                </div>
-                <div className="w-full flex justify-between">
-                    <div className="w-[25px] h-[25px] bg-[#e9ebec] rounded-full"></div>
-                    <div className="w-[25px] h-[25px] bg-[#e9ebec] rounded-full"></div>
-                </div>
+
             </div>
         </div>
     );
