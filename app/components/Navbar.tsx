6c3ddb67981563fbf6b9679898dc507a14ae98a1
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-08-19 07:09:09 +0200

    balls

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -43,20 +43,20 @@ export default function Navbar() {
                     initial={{ fontSize: '24vw' }}
                 >
                     <div className="flex items-end overflow-hidden">
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0, duration: 1, ease: easeInOut }}> L </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.1, duration: 1, ease: easeInOut }}> u </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.2, duration: 1, ease: easeInOut }}> c </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.3, duration: 1, ease: easeInOut }}> a </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.4, duration: 1, ease: easeInOut }}> s </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0, duration: 1, ease: easeInOut }} className="select-none"> L </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.1, duration: 1, ease: easeInOut }} className="select-none"> u </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.2, duration: 1, ease: easeInOut }} className="select-none"> c </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.3, duration: 1, ease: easeInOut }} className="select-none"> a </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.4, duration: 1, ease: easeInOut }} className="select-none"> s </motion.div>
                     </div>
-                    <div className="flex items-start overflow-hidden h-[22vw] lastNameOpacity" style={{ opacity: lastNameOpacity }}>  
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 1, ease: easeInOut }}> H </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.6, duration: 1, ease: easeInOut }}> a </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.7, duration: 1, ease: easeInOut }}> g </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.8, duration: 1, ease: easeInOut }}> f </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.9, duration: 1, ease: easeInOut }}> o </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1, duration: 1, ease: easeInOut }}> s </motion.div>
-                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1.1, duration: 1, ease: easeInOut }}> s </motion.div>
+                    <div className="flex items-start overflow-hidden lastNameOpacity" style={{ opacity: lastNameOpacity }}>  
+                        <motion.div initial={{ y: 360, opacity: 0, }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 1, ease: easeInOut }} className="select-none"> H </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.6, duration: 1, ease: easeInOut }} className="select-none"> a </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.7, duration: 1, ease: easeInOut }} className="select-none"> g </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.8, duration: 1, ease: easeInOut }} className="select-none"> f </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.9, duration: 1, ease: easeInOut }} className="select-none"> o </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1, duration: 1, ease: easeInOut }} className="select-none"> s </motion.div>
+                        <motion.div initial={{ y: 360, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1.1, duration: 1, ease: easeInOut }} className="select-none"> s </motion.div>
                     </div>
                 </motion.h1>
                 <div className="m-4 items-center top-0 right-0 flex gap-10 absolute overflow-hidden">
@@ -64,7 +64,7 @@ export default function Navbar() {
                     <motion.div initial={{ y: 50, }} animate={{ y: 0, }} transition={{ delay: 1.3, duration: 1, ease: easeInOut }}> <Link href="/" className="text-[#e9ebec] bricolage-500">Work</Link> </motion.div>
                     <motion.div initial={{ y: 50, }} animate={{ y: 0, }} transition={{ delay: 1.4, duration: 1, ease: easeInOut }}> <Link href="/" className="text-[#e9ebec] bricolage-500">Insight</Link> </motion.div>
                     <motion.div initial={{ y: 50, }} animate={{ y: 0, }} transition={{ delay: 1.5, duration: 1, ease: easeInOut }}> <Link href="/" className="text-[#e9ebec] bricolage-500">Contact</Link> </motion.div>
-                    <motion.div initial={{ y: 50, }} animate={{ y: 0, }} transition={{ delay: 1.6, duration: 1, ease: easeInOut }}> <div className="bg-[#34383c] hover:bg-[#4c5257] duration-300 py-3 px-4 rounded-full cursor-pointer flex gap-2"><Image src={copy} alt="" className=""></Image><button type="button" className="text-[#e9ebec] bricolage-500 w-full h-full underline">Hello@lhagfoss.studio</button></div> </motion.div>
+                    <motion.div initial={{ y: 50, }} animate={{ y: 0, }} transition={{ delay: 1.6, duration: 1, ease: easeInOut }}> <div className="shadow-xl bg-[#34383c] hover:bg-[#4c5257] duration-300 py-3 px-4 rounded-full cursor-pointer flex gap-2"><Image src={copy} alt="" className=""></Image><button type="button" className="text-[#e9ebec] bricolage-500 w-full h-full underline">Hello@lhagfoss.studio</button></div> </motion.div>
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
 "use client"
 
-import { motion, easeIn, easeInOut, easeOut } from "framer-motion"
+import { motion, easeIn, easeInOut, easeOut, AnimatePresence } from "framer-motion"
 import React, { useEffect, useState, useRef } from "react";
 import Lenis from 'lenis'
 import Image from "next/image";
 
 import Navbar from "@components/Navbar";
 import down from "@assets/down.svg"
+import image1 from "@assets/pexels-pixabay-356056.jpg"
 
 export default function Home() {
     const lenisRef = useRef<Lenis | null>(null);
+    const [time, setTime] = useState(new Date());
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+    
     useEffect(() => {
         window.scrollTo(0, 0);
         const lenis = new Lenis()
@@ -38,26 +50,49 @@ export default function Home() {
     return (
         <div className="bg-w-da-wallpaper w-screen">
             <Navbar />
-            <div className="w-screen h-[80vh]"></div>
-            <div className="w-screen flex flex-col items-center">
-                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2.5, duration: 1, ease: easeInOut }} className="">
-                <a href="#down" onClick={(e) => handleScrollTo(e, "#down")}>
-                    <Image src={down} alt="" className="w-[40px]" />
+            <div className="w-screen h-[100vh]"></div>
+            <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1.7, duration: 1, ease: easeInOut }} className="select-none">
+                <a href="#down" className="m-5 left-0 bottom-0 absolute bg-[#34383c] hover:bg-[#4c5257] duration-300 rounded-full shadow-xl onTop" onClick={(e) => handleScrollTo(e, "#down")}>
+                    <Image src={down} alt="" className="w-[50px]" />
                 </a>
-                </motion.div>
-                {/* <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1.8, duration: 1, ease: easeInOut }} className="">
-                </motion.div> */}
-            </div>
-            <div id="down" className="w-screen h-[15vh]"></div>
-            <div className="w-screen p-10 text-[#e9ebec] text-[4vw] bricolage-500 leading-[0.9]">
-                <div className="bg-[#34383c] hover:bg-[#4c5257] max-w-[250px] min-h-[60px] duration-300 mb-5 py-3 px-4 rounded-full cursor-pointer flex justify-around items-center"><div className="relative ml-4 flex justify-center items-center"><motion.div initial={{ opacity: 0, scale: 0.7 }} animate={{ opacity: [0, 1, 0], scale: 1 }} transition={{ delay: 0, duration: 1, ease: easeInOut, repeat: Infinity }} className="absolute rounded-full min-w-[23px] min-h-[23px] bg-[#1c5e1d] flex justify-center items-center"></motion.div><div className="z-10 rounded-full min-w-[15px] min-h-[15px] bg-[#38c13b]"></div></div><button type="button" className="text-[#e9ebec] text-[1rem] bricolage-500 w-full h-full">Available for Work</button></div>
+            </motion.div>
+            <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 1.8, duration: 1, ease: easeInOut }} className="z-10 m-5 right-0 bottom-0 p-3 px-5 fixed  bg-[#34383c] hover:bg-[#4c5257] duration-300 rounded-full shadow-xl select-none flex justify-center items-center min-w-[120px]">
+                <div className="text-[#e9ebec] bricolage-500 flex">
+                    <AnimatePresence mode="popLayout">
+                        {time.toTimeString().slice(0, 8).split('').map((char, index) => (
+                            <motion.span
+                                key={`${index}-${char}`}
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -20 }}
+                                transition={{ duration: 0.2 }}
+                            >
+                                {char}
+                            </motion.span>
+                        ))}
+                    </AnimatePresence>
+                </div>
+            </motion.div>
+            <div id="down" className="w-screen h-[20vh]"></div>
+            <div className="m-5 text-[#e9ebec] text-[4vw] bricolage-500 leading-[0.9]">
+                <div className="bg-[#34383c] hover:bg-[#4c5257] max-w-[250px] min-h-[50px] duration-300 mb-5 py-3 px-4 rounded-full cursor-pointer flex justify-around items-center"><div className="relative ml-4 flex justify-center items-center"><motion.div initial={{ opacity: 0, scale: 0.7 }} animate={{ opacity: [0, 1, 0], scale: 1 }} transition={{ delay: 0, duration: 1, ease: easeInOut, repeat: Infinity }} className="absolute rounded-full min-w-[23px] min-h-[23px] bg-[#1c5e1d] flex justify-center items-center"></motion.div><div className="z-10 rounded-full min-w-[15px] min-h-[15px] bg-[#38c13b]"></div></div><button type="button" className="text-[#e9ebec] text-[1rem] bricolage-500 w-full h-full">Available for Work</button></div>
                <span className="text-[#646b73] mr-2"> I'm Lucas Hagfoss — a user</span><span className="">experience & interface designer <span className="text-[#646b73] mr-1">from Norway, focusing on building functional websites that will</span> increase conversion <span className="text-[#646b73] mr-1">and help</span> reach customers.</span>
             </div>
-            <div className="w-screen my-[30px] pl-10 text-[#e9ebec] text-[4vw] bricolage-500"><span className="text-[#646b73] mr-2">Latest</span> Projects</div>
-            <div className="w-screen h-screen">
-                <div className="relative translate-x-[-50%] left-[50%] w-[97vw] h-[100vw] grid grid-cols-9 grid-rows-3 gap-5">
-                    <div className="border col-span-6 rounded-[3vw] overflow-hidden"></div>
-                    <div className="border col-span-3 rounded-[3vw] overflow-hidden"></div>
+            <div className="my-[30px] m-5 text-[#e9ebec] text-[3vw] bricolage-500"><span className="text-[#646b73] mr-2">Latest</span> Projects</div>
+            <div className="m-5 h-screen">
+                <div className="relative translate-x-[-50%] left-[50%] h-[110vw] grid grid-cols-9 grid-rows-3 gap-5">
+                    <div className="col-span-6 rounded-[4vw] overflow-hidden relative">
+                        <div className="w-full h-full">
+                            <Image src={image1} alt="" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="absolute backdrop-blur-xl bottom-0 bg-[#11111183] left-0 m-5 w-[96%] h-[20%] rounded-full"></div>
+                    </div>
+                    <div className="col-span-3 rounded-[4vw] overflow-hidden relative group cursor-pointer">
+                        <div className="w-full h-full">
+                            <Image src={image1} alt="" className="w-full h-full object-cover imageBalls transition-scale duration-700" />
+                        </div>
+                        <div className="absolute backdrop-blur-xl bottom-0 bg-[#11111183] left-0 m-5 w-[93%] h-[20%] rounded-full group-hover transition-opacity duration-500"></div>
+                    </div>
                     <div className="border col-span-4 rounded-[3vw] overflow-hidden"></div>
                     <div className="border col-span-5 row-span-2 rounded-[3vw] overflow-hidden"></div>
                     <div className="border col-span-2 rounded-[3vw] overflow-hidden"></div>
